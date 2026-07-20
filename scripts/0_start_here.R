5c3e85ea34918b931db5df5aa1b3fa590e2ee134
Marshall A. Taylor <Marshall-Soc@users.noreply.github.com>
2026-07-20 09:53:02 -0600

    Update 0_start_here.R
    Quick update

--- scripts/0_start_here.R
+++ scripts/0_start_here.R
@@ -1,8 +1,8 @@
 
 # source("scripts/1_install_packages.R")
 
-source("scripts/1_table_descriptives.R")
-source("scripts/2_plots_descriptives_fig1_simplifed.R")
-source("scripts/2_plots_descriptives_figs_combined.R")
-source("scripts/3_glm_regression_olr_robustness.R")
-source("scripts/4_plots_descriptives_figs_likerts_appendix.R")
+source("1_table_descriptives.R")
+source("2_plots_descriptives_fig1_simplifed.R")
+source("2_plots_descriptives_figs_combined.R")
+source("3_glm_regression_olr_robustness.R")
+source("4_plots_descriptives_figs_likerts_appendix.R")
